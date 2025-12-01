3a7eb039cadccc9e9e1e83de841c072f7dd2188d
Germanium Sculk <germaniumsculk@gmail.com>
2025-12-01 22:57:20 +0600

    qpnp : enable Brightness toggle...

--- drivers/leds/leds-qpnp-flash-v2.c
+++ drivers/leds/leds-qpnp-flash-v2.c
@@ -1726,7 +1726,17 @@ static int qpnp_flash_led_switch_set(struct flash_switch_data *snode, bool on)
 		}
 		return 0;
 	}
-
+	
+/* PATCH: Force update current from brightness value before enabling */
+	for (i = 0; i < led->num_fnodes; i++) {
+		if ((snode->led_mask & BIT(led->fnode[i].id)) &&
+			led->fnode[i].cdev.brightness > 0) {
+			qpnp_flash_led_node_set(&led->fnode[i],
+				led->fnode[i].cdev.brightness);
+		}
+	}
+	/* END PATCH */
+	
 	val = 0;
 	for (i = 0; i < led->num_fnodes; i++) {
 		if (!led->fnode[i].led_on ||
